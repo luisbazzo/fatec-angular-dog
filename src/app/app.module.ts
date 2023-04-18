c5d1f3ade3a121e631fe68bd52b41f7e3089358e
luisbazzo <luis.o.c.bazzo@gmail.com>
2023-04-18 11:27:17 -0300

    Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GenerateDogComponent } from './generate-dog/generate-dog.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/generate-dog/generate-dog.component.ts
+++ src/app/generate-dog/generate-dog.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { DogService } from './../dog.service';
+import { Dog } from './../dog';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-generate-dog',
   templateUrl: './generate-dog.component.html',
   styleUrls: ['./generate-dog.component.css']
 })
-export class GenerateDogComponent {
+export class GenerateDogComponent implements OnInit{
 
+  constructor(private dogService : DogService) { }
+
+  ngOnInit(): void {
+    this.loadDog();
+  }
+  loadDog(){
+    this.dogService.getRandomDog().subscribe(
+      {
+        next : dog => this.dog = dog
+      }
+    );
+  }
+
+  dog : Dog = {} as Dog;
+
+getName() : string []{
+  return this.dog.message.split("/")[4].split("-");
+}
 }
